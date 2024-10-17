e52dc247b543b89604ea492bc8579419471f39df
stevebiczyk <stevebiczyk@gmail.com>
2024-10-17 23:19:50 +0100

    added TileContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { PathfinderProvider } from "./context/PathfinderContext";
+import { TileProvider } from "./context/TileContext";
 
 export default function App() {
   return (
     <PathfinderProvider>
-      <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
-        Hello world!
-      </h1>
+      <TileProvider>
+        <h1 className="text-3xl font-bold underline h-screen w-screen bg-blue-500">
+          Hello world!
+        </h1>
+      </TileProvider>
     </PathfinderProvider>
   );
 }
---
+++ src/context/TileContext.tsx
@@ -0,0 +1,26 @@
+import { createContext, ReactNode, useState } from "react";
+import { TileType } from "../utils/types";
+
+interface TileContextInterface {
+  startTile: TileType;
+  setStartTile: (startTile: TileType) => void;
+  endTile: TileType;
+  setEndTile: (endTile: TileType) => void;
+}
+
+export const TileContext = createContext<TileContextInterface | undefined>(
+  undefined
+);
+
+export const TileProvider = ({ children }: { children: ReactNode }) => {
+  const [startTile, setStartTile] = useState<TileType>({ START_TILE_CONFIG });
+  const [endTile, setEndTile] = useState<TileType>({ END_TILE_CONFIG });
+
+  return (
+    <TileContext.Provider
+      value={{ startTile, setStartTile, endTile, setEndTile }}
+    >
+      {children}
+    </TileContext.Provider>
+  );
+};
