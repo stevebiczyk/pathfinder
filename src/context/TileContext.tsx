496d7f432f666457c9673fa0ccd7aa25c3f1a82c
stevebiczyk <stevebiczyk@gmail.com>
2024-12-12 16:18:45 +0000

    added dijkstra path finder plus error fixes in other algorithms

--- src/context/TileContext.tsx
+++ src/context/TileContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from "react";
+import { ReactNode, createContext, useState } from "react";
 import { TileType } from "../utils/types";
 import { END_TILE_CONFIG, START_TILE_CONFIG } from "../utils/constants";
 
@@ -14,12 +14,17 @@ export const TileContext = createContext<TileContextInterface | undefined>(
 );
 
 export const TileProvider = ({ children }: { children: ReactNode }) => {
-  const [startTile, setStartTile] = useState<TileType>({ START_TILE_CONFIG });
-  const [endTile, setEndTile] = useState<TileType>({ END_TILE_CONFIG });
+  const [startTile, setStartTile] = useState<TileType>(START_TILE_CONFIG);
+  const [endTile, setEndTile] = useState<TileType>(END_TILE_CONFIG);
 
   return (
     <TileContext.Provider
-      value={{ startTile, setStartTile, endTile, setEndTile }}
+      value={{
+        startTile,
+        setStartTile,
+        endTile,
+        setEndTile,
+      }}
     >
       {children}
     </TileContext.Provider>
--- src/lib/algorithms/pathfinding/bfs.ts
+++ src/lib/algorithms/pathfinding/bfs.ts
@@ -8,10 +8,10 @@ export const bfs = (grid: GridType, startTile: TileType, endTile: TileType) => {
   const base = grid[startTile.row][startTile.col];
   base.distance = 0;
   base.isTraversed = true;
-  const unTraversed = [base];
+  const untraversedTiles = [base];
 
-  while (unTraversed.length) {
-    const tile = unTraversed.shift() as TileType;
+  while (untraversedTiles.length) {
+    const tile = untraversedTiles.shift() as TileType;
     if (tile.isWall) continue;
     if (tile.distance === Infinity) break;
     tile.isTraversed = true;
@@ -20,11 +20,11 @@ export const bfs = (grid: GridType, startTile: TileType, endTile: TileType) => {
 
     const neighbours = getUntraversedNeighbours(grid, tile);
     for (let i = 0; i < neighbours.length; i += 1) {
-      if (!isInQueue(neighbours[i], unTraversed)) {
+      if (!isInQueue(neighbours[i], untraversedTiles)) {
         const neighbour = neighbours[i];
         neighbour.distance = tile.distance + 1;
         neighbour.parent = tile;
-        unTraversed.push(neighbour);
+        untraversedTiles.push(neighbour);
       }
     }
   }
@@ -37,3 +37,59 @@ export const bfs = (grid: GridType, startTile: TileType, endTile: TileType) => {
   }
   return { traversedTiles, path };
 };
+
+// export const bfs = (grid: GridType, startTile: TileType, endTile: TileType) => {
+//   // Check for undefined or invalid startTile
+//   if (
+//     !startTile ||
+//     startTile.row === undefined ||
+//     startTile.col === undefined
+//   ) {
+//     throw new Error("Invalid startTile: row and col must be defined");
+//   }
+
+//   const base = grid[startTile.row]?.[startTile.col];
+//   if (!base) {
+//     throw new Error(
+//       `Start tile (${startTile.row}, ${startTile.col}) is outside the grid bounds.`
+//     );
+//   }
+
+//   base.distance = 0;
+//   base.isTraversed = true;
+
+//   const traversedTiles: TileType[] = [];
+//   const untraversedTiles = [base];
+
+//   while (untraversedTiles.length) {
+//     const tile = untraversedTiles.shift() as TileType;
+
+//     if (tile.isWall) continue;
+//     if (tile.distance === Infinity) break;
+
+//     tile.isTraversed = true;
+//     traversedTiles.push(tile);
+
+//     if (isEqual(tile, endTile)) break;
+
+//     const neighbours = getuntraversedNeighbours(grid, tile) || [];
+//     for (let i = 0; i < neighbours.length; i += 1) {
+//       if (!isInQueue(neighbours[i], untraversedTiles)) {
+//         const neighbour = neighbours[i];
+//         neighbour.distance = tile.distance + 1;
+//         neighbour.parent = tile;
+//         untraversedTiles.push(neighbour);
+//       }
+//     }
+//   }
+
+//   const path = [];
+//   let tile = grid[endTile.row]?.[endTile.col];
+//   while (tile !== null && tile.parent) {
+//     tile.isPath = true;
+//     path.unshift(tile);
+//     tile = tile.parent;
+//   }
+
+//   return { traversedTiles, path };
+// };
